b9798f2f351a8af25fc478994ae0951f3238fd09
Aayush Bashyal <aasyuhbashyal92@gmail.com>
2026-09-24 20:10:52 +0545

    Initial commit

---
+++ src/main/java/net/bikash/manhunt/Manhunt.java
@@ -0,0 +1,19 @@
+package net.bikash.manhunt;
+
+import net.fabricmc.api.ModInitializer;
+
+import net.minecraft.resources.Identifier;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Manhunt implements ModInitializer {
+	public static final String MOD_ID = "manhunt";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+
+}
---
+++ src/main/java/net/bikash/manhunt/ManhuntDataGenerator.java
@@ -0,0 +1,11 @@
+package net.bikash.manhunt;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class ManhuntDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
