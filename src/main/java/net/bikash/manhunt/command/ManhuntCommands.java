bad73927f78de7f77cadaff714717e2071f1802c
Aayush Bashyal <aayushbashyal92@gmail.com>
2026-09-25 13:03:24 +0545

    registering new minecraft commands for addingthe manhunt time

--- src/main/java/net/bikash/manhunt/command/ManhuntCommands.java
+++ src/main/java/net/bikash/manhunt/command/ManhuntCommands.java
@@ -57,8 +57,13 @@ public class ManhuntCommands {
                 ))
                 .then(Commands.literal("status").executes(context -> {
                             if (Manhunt.Game.isRunning()) {
+                                long elapsed = Manhunt.Game.getElapsedTime();
+                                long totalseconds = elapsed/1000;
+                                long minutes = totalseconds/60;
+                                long  seconds = totalseconds%60;
+
                                 context.getSource().sendSuccess(
-                                        () -> Component.literal("Manhunt is Currently running!"),
+                                        () -> Component.literal("Manhunt is Currently running!  Time: "+minutes+ "min " + seconds + "sec"),
                                         false
                                 );
 
